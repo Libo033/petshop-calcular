f7cc93cbe1a8b57e4a700cc8efd57546b1873272
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-08-17 11:34:09 -0300

    Cambio de rumbo

--- src/pages/api/v1/productos/[id]/index.tsx
+++ src/pages/api/v1/productos/[id]/index.tsx
@@ -2,7 +2,10 @@ import clientPromise from "@/Libs/databases/mongodb";
 import { Db, MongoClient, ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
     // Obtener un solo proveedor
     try {
@@ -22,10 +25,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     }
   }
-  if (req.method === "PUT") {   // Modificar un producto (por marca - por proveedor)
-    
+  if (req.method === "PUT") {
+    // Modificar un producto (por marca - por proveedor)
+    try {
+      const client: MongoClient = await clientPromise;
+      const db: Db = client.db("petshop");
+      const id: string | undefined = req.query.id?.toString();
+
+      const modificar_producto = {
+        nombre: req.body.nombre,
+        marca: req.body.marca,
+        proveedor: req.body.proveedor,
+        precio_compra: req.body.precio_compra,
+        precio_venta_kg: req.body.precio_venta_kg,
+        precio_venta_bolsa: req.body.precio_venta_bolsa,
+        kg_bolsa: req.body.kg_bolsa,
+        ult_mod: new Date().toISOString().slice(0, 10).toString(),
+      };
+
+      const producto_modificado = await db
+        .collection("productos")
+        .updateOne({ _id: new ObjectId(id) }, { $set: modificar_producto });
+
+      return res
+        .status(200)
+        .json({ modificada: producto_modificado.acknowledged });
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(500).json({ error: error.message });
+      }
+    }
   }
-  if (req.method === "DELETE") {   // Borrar un producto
+  if (req.method === "DELETE") {
+    // Borrar un producto
     try {
       const client: MongoClient = await clientPromise;
       const db: Db = client.db("petshop");
